c20846bebd77fa3b982399379fb9fce453d85390
thomas.aittaleb <thomas.aittaleb@3e47b0b6-2e77-c9ff-9150-02e83688887a>
2010-11-17 14:43:20 +0000

    Documentation Doxygen Target added. Now, the documentation target generate HTML, Latex and DocSet. All is full compatible with Xcode.
    Added Server class declaration.

---
+++ trunk/TextMiningServer/Server.cc
@@ -0,0 +1,10 @@
+/**
+ ** \file Server.cc
+ ** \author Thomas Ait-taleb
+ ** \version 0.1
+ ** \date 17 novembre 2010
+ ** \brief Implementation of Server class
+ **/
+
+#include "Server.hh"
+
---
+++ trunk/TextMiningServer/main.cc
@@ -0,0 +1,22 @@
+/**
+ ** \file main.cc
+ ** \author Thomas Ait-taleb
+ ** \version 0.1
+ ** \date 17 novembre 2010
+ ** \brief Start point of the server
+ **/
+
+#include <iostream>
+
+int main (int argc, char * const argv[]) {
+
+	// Arguments manager
+	
+	// Dictionary loader
+	
+	// Server initialization
+	
+	// Server runloop
+	
+    return 0;
+}
