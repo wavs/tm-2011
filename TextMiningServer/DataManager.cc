22f04184bf9dc43caa960f62497a48d490a705ae
thomas.aittaleb <thomas.aittaleb@3e47b0b6-2e77-c9ff-9150-02e83688887a>
2010-11-19 16:15:03 +0000

    Added declaration of DataManager class. Added Singleton template class.
    TODO:
    + Implement DataManager class
    + Declare & Implement ClientFactory class

---
+++ TextMiningServer/DataManager.cc
@@ -0,0 +1,40 @@
+/**
+ ** \file DataManager.cc
+ ** \author Thomas Ait-taleb
+ ** \version 0.1
+ ** \date 17 novembre 2010
+ ** \brief Implémentation de la classe DataManager
+ **/
+
+#include "DataManager.hh"
+
+DataManager::DataManager ()
+{}
+
+DataManager::~DataManager ()
+{}
+
+void
+DataManager::loadFile(string filename)
+{
+	// FIXME
+}
+
+void
+DataManager::dump()
+{
+	// FIXME
+}
+
+TResultsDictionnary
+*DataManager::browseDataWithOptions(string word, int dist)
+{
+	// FIXME
+}
+
+ostream&
+DataManager::exportJSON(vector<s_result> *results)
+{
+	// FIXME
+}
+
