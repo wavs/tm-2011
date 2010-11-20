a2ec7a05a3ce0b0a60d2c89663ca838a15b48668
guilbep <guilbep@3e47b0b6-2e77-c9ff-9150-02e83688887a>
2010-11-20 17:20:42 +0000

    Trie Problem allocation memory

--- trunk/TextMiningCompiler/TMtrie.h
+++ trunk/TextMiningCompiler/TMtrie.h
@@ -12,7 +12,13 @@
 # include "TMalphabetMap.h"
 
 # define SIZE_ALPHABET 127          /* Using a-z and space */
-# define MAXDEPTH 42                 /* Supporting 20 character depth of dictionary */
+# define MAXDEPTH 42                /* Supporting 20 character depth of dictionary */
+# define MB_512 536870912			/*	536 870 912 */
+# define MB_256 268435456
+# define MB_128 134217728
+# define MB_64 67108864
+# define MB_32 32554432
+# define MB_16 16777216
 
 
 /*
@@ -32,20 +38,24 @@ class Trie {
 	private:
 		char*		trie;
 	private:
-	/* We need to put some information about the structure at the begining of our Trie */
-		void		initTrieWithAlphabet(AlphabetMap alphaMap);
 	/* we allocate as much as we can, because we don't know yet, how much we will need */
 		void		initTrieMemory(unsigned long int sizeNeeded); 
+	/* We need to put some information about the structure at the begining of our Trie */
+		void		initTrieHeaderWithAlphabet(AlphabetMap alphaMap);
 	/* Once we've put every word we needed in our Trie, we can reduce it, so it'll take less memory */
 		void		resizeTrieMemory(void);
-	
-		void		addWord(std::string word, );
+	/* add a word to the trie with the frequence */
+		void		addWord(std::string word, unsigned long int frequence);
+	/* construct the Trie */
+		void		parseFileToTrie(std::string filePath);
 	public:
-		Trie(unsigned long int sizeNeeded, AlphabetMap alphaMap);
+		Trie(unsigned long int sizeNeeded, AlphabetMap alphaMap, std::string filePath);
 		~Trie();
 	public:
 		int			getFrequence(std::string word);
 		int			getFrequence(char		*word);
+		/* Dump the structure char* trie to the file */
+		int			compileTrie(std::string destinationPath);
 	
 };
 #endif /* !TMTRIE_H_ */
